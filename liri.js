f05026e506445dbd7701885ac5f62d2cd535ae28
Paul Black <pblack67@comcast.net>
2019-05-13 08:42:33 -0500

    Basic command structure

--- liri.js
+++ liri.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+require("dotenv").config();
+const keys = require("./keys.js");
+
+console.log(process.argv);
+
+switch(process.argv[2]) {
+    case "concert-this":
+        console.log("concert-this");
+        break;
+
+    case "spotify-this-song":
+        console.log("spotify-this-song");
+        break;
+    
+    case "movie-this":
+        console.log("movie-this");
+        break;
+    
+    case "do-what-it-says":
+        console.log("do-what-it-says");
+        break;
+
+    default:
+        console.log("Please enter one of the following commands: concert-this, spotify-this-song, movie-this, do-what-it-says");
+    break;
+}
+
